65b006d161ca55ae7e9ffc332a65de943ac38b89
Jesper <jesper.olsson@iths.se>
2021-11-23 17:48:22 +0100

    refactored stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,253 @@
-const cartHandler = () => {
-  const buttons = document.querySelectorAll(".product-card__button")
-  const cartView = document.querySelector(".cart-view")
-  const closeView = document.querySelector(".close-view")
-  const openView = document.querySelector(".open-view")
-  cartView.style.display = "none"
-  openView.style.display = "none"
-  const cartItems = []
-  for (const button of buttons) {
-    button.addEventListener("click", (e) => {
-      const cartElements = document.querySelectorAll(".cartItems")
-      for (const elem of cartElements) {
-        elem.style.display = "block"
-      }
-      openView.style.display = "none"
-        closeView.style.display  ="block"
-      if (cartItems.includes(e.target.value)) {
-        //inkrementera
-        cartView.style.display = "block"
-      }else{
-        cartView.style.display = "block"
-        cartItems.push(e.target.value)
-        localStorage.setItem("cachedItems", cartItems)
-        document.querySelector(".cart-view").insertAdjacentHTML("beforeend", `
-        <li class="cartItems">${e.target.value} x1</li>
-        `)
-        console.log(localStorage)
-      }
-    })
-  }
-  closeView.addEventListener("click", () => {
-    const cartElements = document.querySelectorAll(".cartItems")
-    for (const elem of cartElements) {
-      elem.style.display = "none"
-    }
-    closeView.style.display = "none"
-    openView.style.display = "block"
-  })
-  openView.addEventListener("click", () => {
-    const cartElements = document.querySelectorAll(".cartItems")
-    for (const elem of cartElements) {
-      elem.style.display = "block"
-    }
-    openView.style.display = "none"
-    closeView.style.display = "block"
-  })
+let cart;
+let productList
+
+const saveCart = () => {
+  const cartString = JSON.stringify(cart)
+  localStorage.setItem("cart", cartString)
 }
+
+const loadCart = () => {
+  const savedCart = localStorage.getItem("cart")
+  if (savedCart) {
+    cart = JSON.parse(savedCart)
+  } else {
+    cart = []
+  }
+}
+
+const setupInitialState = () => {
+  productList = [
+    { name: "Extraordinary Fester Flea", img: "images/Fleas.png", price: 100 },
+    { name: "Exquisite Glyphid Exploder", img: "images/Glyphid_exploder.png", price: 10 },
+    { name: "Outstanding Glyphid Slasher", img: "images/Glyphid_Slasher.png", price: 1337 },
+    { name: "Remarkable Glyphid Swarmer", img: "images/Glyphid_swarmer.png", price: 1337 },
+    { name: "Unprecedented Golden Lootbug", img: "images/Golden_LootBug.png", price: 1337 },
+    { name: "Exceptional Lootbug", img: "images/LootBug.png", price: 1337 }
+  ]
+  loadCart()
+}
+
+
+const increase = (li, item) => {
+  document.querySelector(".num-items").innerText++
+  item.amount++
+  li.querySelector("p").innerText = `${item.amount}x ${item.name}`
+  let inCart = null
+  for (let needle of cart) {
+    if (item.name == needle.name) {
+      inCart = needle;
+      break;
+    }
+  }
+  if (inCart) {
+    inCart.amount++
+  }
+ 
+  console.log(item)
+  console.log(cart)
+
+  saveCart()
+  // newFunc(li, item)---------
+}
+
+// const newFunc = (li, item) => {-------------
+//   const addToCartBtn = productArticle.querySelector(".product-card__button")
+//   addToCartBtn.addEventListener("click", () => {
+//     document.querySelector(".num-items").innerText++
+//     item.amount++
+//     li.querySelector("p").innerText = `${item.amount}x ${item.name}`
+//     let inCart = null
+//     for (let needle of cart) {
+//       if (item.name == needle.name) {
+//         inCart = needle;
+//         break;
+//       }
+//     }
+//     if (inCart) {
+//       inCart.amount++
+//     }
+//   })
+// }
+
+const decrease = (li, item) => {
+  document.querySelector(".num-items").innerText--
+  item.amount--
+  if (item.amount <= 0) {
+    cart = cart.filter(i => i.name != item.name)
+    li.remove()
+  } else {
+    li.querySelector("p").innerText = `${item.amount}x ${item.name}`
+    let inCart = null
+    for (let needle of cart) {
+      if (item.name == needle.name) {
+        inCart = needle;
+        break;
+      }
+    }
+    if (inCart) {
+      console.log(item)
+      console.log(cart)
+      inCart.amount--
+    }
+  }
+  saveCart()
+}
+
+const createCartItem = (item) => {
+  const li = document.createElement("li")
+  console.log(item)
+  li.innerHTML = `
+  <p>${item.amount}x ${item.name}</p>
+  <div class="controls">
+  <button class="decrease">-</button>
+  <button class="increase">+</button>
+  </div>
+  `
+  li.querySelector(".increase")
+  .addEventListener("click", () => increase(li, item))
+  
+  li.querySelector(".decrease")
+  .addEventListener("click", () => decrease(li, item))
+  // console.error(li)
+
+  // document.querySelector(".product-card__button").addEventListener("click", () => increase(li, item))
+  
+  return li
+}
+
+const renderCart = () => {
+  const cartContainer = document.querySelector(".cart")
+  for (let item of cart) {
+    const li = createCartItem(item)
+    cartContainer.append(li)
+  }
+}
+
+const getCartAmount = () => {
+  let sum = 0
+  for (let item of cart) {
+    sum += item.amount
+  }
+  return sum
+}
+
+const createProduct = (product) => {
+  const productArticle = document.createElement("article")
+  productArticle.classList.add("product")
+  
+  productArticle.innerHTML = `
+  <div class="product-image-container">
+  <img src=${product.img} alt="" srcset="">
+  </div>
+  <p class="name">${product.name}</p>
+  <p class="price">${product.price}:-</p>
+  <button class="product-card__button">ADD TO CART</button>
+  `
+  const addToCartBtn = productArticle.querySelector(".product-card__button")
+  addToCartBtn.addEventListener("click", () => {
+    addToCart(product)
+  })
+  return productArticle
+}
+
+const renderProductList = () => {
+  const productsContainer = document.querySelector("section.products")
+  for (let product of productList) {
+    const productElement = createProduct(product)
+    productsContainer.append(productElement)
+  }
+  const numItems = document.querySelector(".num-items")
+  numItems.classList.add("active")
+  numItems.innerText = getCartAmount()
+}
+
+const renderNumItems = () => {
+  const numItems = document.querySelector(".num-items")
+  numItems.classList.add("active")
+  numItems.innerText = getCartAmount()
+}
+
+addToCart = (product) => {
+  // let inCart = cart.find(item => item.name == product.name)
+  let inCart = null
+  console.log(product)
+  for (let needle of cart) {
+    if (product.name == needle.name) {
+      inCart = needle;
+      break;
+    }
+  }
+  if (inCart) {
+  //   for (let item of cart) {
+  //     const li = createCartItem(item)
+  // console.log(li)
+  // li.querySelector("p").innerText = `${product.amount}x ${product.name}`
+  //   }
+    inCart.amount++
+  } else {
+    const orderItem = { ...product, amount: 1 }
+    cart.push(orderItem)
+  }
+
+  saveCart()
+  renderNumItems()
+}
+
+// const cartHandler = () => {
+//   const buttons = document.querySelectorAll(".product-card__button")
+//   const cartView = document.querySelector(".cart-view")
+//   const closeView = document.querySelector(".close-view")
+//   const openView = document.querySelector(".open-view")
+//   cartView.style.display = "none"
+//   openView.style.display = "none"
+//   // const cartItems = []
+//   // localStorage.getItem("cachedItems")
+//   for (const button of buttons) {
+//     button.addEventListener("click", (e) => {
+//       const cartElements = document.querySelectorAll(".cartItems")
+//       for (const elem of cartElements) {
+//         elem.style.display = "block"
+//       }
+//       openView.style.display = "none"
+//         closeView.style.display  ="block"
+//       if (cartItems.includes(e.target.value)) {
+//         //inkrementera och plussa på items i cart nav
+//         document.querySelector(".counter").innerText++
+//         cartView.style.display = "block"
+//       }else{
+//         cartView.style.display = "block"
+//         cartItems.push(e.target.value)
+//         localStorage.setItem("cachedItems", cartItems)
+//         document.querySelector(".cart-view").insertAdjacentHTML("beforeend", `
+//         <li class="cartItems">${e.target.value} <button class="increment">-</button><span class="counter">  1  </span><button class="decrement">+</button></li>
+//         `)
+//         console.log(localStorage)
+//       }
+//     })
+//   }
+//   closeView.addEventListener("click", () => {
+//     const cartElements = document.querySelectorAll(".cartItems")
+//     for (const elem of cartElements) {
+//       elem.style.display = "none"
+//     }
+//     closeView.style.display = "none"
+//     openView.style.display = "block"
+//   })
+//   openView.addEventListener("click", () => {
+//     const cartElements = document.querySelectorAll(".cartItems")
+//     for (const elem of cartElements) {
+//       elem.style.display = "block"
+//     }
+//     openView.style.display = "none"
+//     closeView.style.display = "block"
+//   })
+// }
 
 const mapper = () => {
   const mapInfo = document.querySelector(".map-info")
   document.querySelector(".map map").addEventListener("click", (e) => {
     mapInfo.classList.add("map-info-box")
-    mapInfo.innerHTML=""
+    mapInfo.innerHTML = ""
     mapInfo.insertAdjacentHTML("afterbegin", `
     <h1>${e.target.title}</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ab doloremque, hic similique mollitia voluptas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ab doloremque, hic similique mollitia voluptas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ab doloremque, hic similique mollitia voluptas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ab doloremque, hic similique mollitia voluptas.
@@ -58,16 +255,15 @@ const mapper = () => {
     `)
 
   })
-checkPos()
+  checkPos()
 }
 
 const checkPos = () => {
   const mapInfo = document.querySelector(".map-info")
   document.addEventListener("scroll", () => {
-    console.log(window.scrollY)
-    if(window.scrollY < 915 || window.scrollY > 2400) {
+    if (window.scrollY < 915 || window.scrollY > 2400) {
       mapInfo.style.display = "none";
-    }else{
+    } else {
       mapInfo.style.display = "block";
     }
   })
@@ -78,7 +274,7 @@ const fetchArticles = async () => {
     const response = await fetch("https://api.spaceflightnewsapi.net/v3/articles?_limit=3")
     const data = await response.json()
     return data
-  } catch (error){
+  } catch (error) {
     console.error(error)
   }
 }
@@ -129,16 +325,19 @@ const renderArticles = async () => {
 //   })
 // }
 
-  
+
 
 const main = () => {
-cartHandler()
-mapper()
-renderArticles()
-console.log(localStorage.getItem("cachedItems"))
+  // cartHandler()
+  loadCart()
+  renderCart()
+  setupInitialState()
+  renderProductList()
+  mapper()
+  renderArticles()
 }
 main()
 
 /*
-add map section link in nav svgs  
+add map section link in nav svgs
 */
